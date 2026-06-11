78de768b17da91598a786dc1d91de4682b74cffd
Prajwal R <prajwalr463@gmail.com>
2026-06-11 21:05:09 +0530

    Add files via upload

---
+++ SQLOnlineRetailDB.sql
@@ -0,0 +1,307 @@
+/*
+	=========================================================
+	project for a "Fictional Online Retail Company". 
+	=========================================================
+
+Project Overview: 
+A.	Database Design
+	-- Database Name: OnlineRetailDB
+
+B.	Tables:
+	-- Customers: Stores customer details
+	-- Products: Stores product details
+	-- Orders: Stores order details
+	-- OrderItems: Stores details of each item in an order
+	-- Categories: Stores product categories
+
+C.	Insert Sample Data:
+	-- Populate each table with sample data
+
+D. Write Queries:
+	-- Retrieve data (e.g., customer orders, popular products).
+	-- Perform aggregations (e.g., total sales, average order value).
+	-- Join tables for comprehensive reports.
+	-- Use subqueries and common table expressions (CTEs).
+*/
+
+--start
+
+-- Before creating check the table is there in system or not if there drop it and creat or it will show error
+
+IF DB_ID ('OnlineRetailDB') IS NOT NULL
+BEGIN
+    DROP DATABASE OnlineRetailDB;
+END
+GO
+
+-- Create the database
+CREATE DATABASE OnlineRetailDB;
+GO
+
+-- Use the database
+USE OnlineRetailDB;
+Go
+
+---drop the table if excit then create
+
+IF OBJECT_ID('dbo.Categories', 'U') IS NOT NULL
+    DROP TABLE dbo.Categories;
+GO
+
+
+-- Create the Customers table
+CREATE TABLE Customers (
+	CustomerID INT PRIMARY KEY IDENTITY(1,1),
+	FirstName NVARCHAR(50),
+	LastName NVARCHAR(50),
+	Email NVARCHAR(100),
+	Phone NVARCHAR(50),
+	Address NVARCHAR(255),
+	City NVARCHAR(50),
+	State NVARCHAR(50),
+	ZipCode NVARCHAR(50),
+	Country NVARCHAR(50),
+	CreatedAt DATETIME DEFAULT GETDATE()
+);
+
+---drop the table if excit then create
+
+IF OBJECT_ID('dbo.Products', 'U') IS NOT NULL
+    DROP TABLE dbo.Products;
+GO
+
+-- Create the Products table
+CREATE TABLE Products (
+	ProductID INT PRIMARY KEY IDENTITY(1,1),
+	ProductName NVARCHAR(100),
+	CategoryID INT,
+	Price DECIMAL(10,2),
+	Stock INT,
+	CreatedAt DATETIME DEFAULT GETDATE()
+);
+---drop the table if excit then create
+
+IF OBJECT_ID('dbo.Categories', 'U') IS NOT NULL
+    DROP TABLE dbo.Categories;
+GO
+
+
+-- Create the Categories table
+CREATE TABLE Categories (
+	CategoryID INT PRIMARY KEY IDENTITY(1,1),
+	CategoryName NVARCHAR(100),
+	Description NVARCHAR(255)
+);
+--drop the table if excit then create
+
+IF OBJECT_ID('dbo.Orders', 'U') IS NOT NULL
+    DROP TABLE dbo.Orders;
+GO
+
+-- Create the Orders table
+CREATE TABLE Orders (
+	OrderID INT PRIMARY KEY IDENTITY(1,1),
+	CustomerID INT,
+	OrderDate DATETIME DEFAULT GETDATE(),
+	TotalAmount DECIMAL(10,2),
+	FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+--drop the table if excit then create
+
+IF OBJECT_ID('dbo.OrderItems', 'U') IS NOT NULL
+    DROP TABLE dbo.OrderItems;
+GO
+
+-- Create the OrderItems table
+CREATE TABLE OrderItems (
+	OrderItemID INT PRIMARY KEY IDENTITY(1,1),
+	OrderID INT,
+	ProductID INT,
+	Quantity INT,
+	Price DECIMAL(10,2),
+	FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
+	FOREIGN KEY (OrderId) REFERENCES Orders(OrderID)
+);
+
+-- Insert sample data into Categories table
+INSERT INTO Categories (CategoryName, Description) 
+VALUES 
+('Electronics', 'Devices and Gadgets'),
+('Clothing', 'Apparel and Accessories'),
+('Books', 'Printed and Electronic Books');
+
+-- Insert sample data into Products table
+INSERT INTO Products(ProductName, CategoryID, Price, Stock)
+VALUES 
+('Smartphone', 1, 699.99, 50),
+('Laptop', 1, 999.99, 30),
+('T-shirt', 2, 19.99, 100),
+('Jeans', 2, 49.99, 60),
+('Fiction Novel', 3, 14.99, 200),
+('Science Journal', 3, 29.99, 150);
+
+-- Insert sample data into Customers table
+INSERT INTO Customers(FirstName, LastName, Email, Phone, Address, City, State, ZipCode, Country)
+VALUES 
+('Sameer', 'Khanna', 'sameer.khanna@example.com', '123-456-7890', '123 Elm St.', 'Springfield', 
+'IL', '62701', 'USA'),
+('Jane', 'Smith', 'jane.smith@example.com', '234-567-8901', '456 Oak St.', 'Madison', 
+'WI', '53703', 'USA'),
+('harshad', 'patel', 'harshad.patel@example.com', '345-678-9012', '789 Dalal St.', 'Mumbai', 
+'Maharashtra', '41520', 'INDIA');
+
+-- Insert sample data into Orders table
+INSERT INTO Orders(CustomerId, OrderDate, TotalAmount)
+VALUES 
+(1, GETDATE(), 719.98),
+(2, GETDATE(), 49.99),
+(3, GETDATE(), 44.98);
+
+-- Insert sample data into OrderItems table
+INSERT INTO OrderItems(OrderID, ProductID, Quantity, Price)
+VALUES 
+(1, 1, 1, 699.99),
+(1, 3, 1, 19.99),
+(2, 4, 1,  49.99),
+(3, 5, 1, 14.99),
+(3, 6, 1, 29.99);
+
+
+--Q1 Retrieve all orders for a specific customer 1
+	SELECT *
+	FROM Orders o
+	JOIN OrderItems oi ON o.OrderId = oi.OrderID
+	JOIN Products p ON oi.ProductID = p.ProductID
+	WHERE o.CustomerID = 1;
+
+-- Q2 Find the total sales for each product
+SELECT p.ProductID, p.ProductName, SUM(oi.Quantity * oi.Price) AS TotalSales
+FROM OrderItems oi
+JOIN Products p 
+ON oi.ProductID = p.ProductID
+GROUP BY p.ProductID, p.ProductName
+ORDER BY TotalSales DESC;
+
+
+--Q3: Calculate the average order value
+SELECT AVG(TotalAmount) AS AverageValueoforder
+FROM Orders;
+
+--Q4: List the top 3 customers by total spending
+SELECT CustomerID, FirstName, LastName, TotalSpent, rn
+FROM
+(SELECT c.CustomerID, c.FirstName, c.LastName, SUM(o.TotalAmount) AS TotalSpent,
+ROW_NUMBER() OVER (ORDER BY SUM(o.TotalAmount) DESC) AS rn
+FROM Customers c
+JOIN Orders o
+ON c.CustomerID = o.CustomerId
+GROUP BY c.CustomerID, c.FirstName, c.LastName)
+sub WHERE rn <= 3;
+
+--Q5: Retrieve the most popular product category
+SELECT CategoryID, CategoryName, TotalQuantitySold, rn
+FROM (
+	SELECT c.CategoryID, c.CategoryName, SUM(oi.Quantity) AS TotalQuantitySold,
+	ROW_NUMBER() OVER (ORDER BY SUM(oi.Quantity) DESC) AS rn
+	FROM OrderItems oi
+	JOIN Products p 
+	ON oi.ProductID = p.ProductID
+	JOIN Categories c
+	ON p.CategoryID = c.CategoryID
+	GROUP BY c.CategoryID, c.CategoryName) sub
+	WHERE rn = 1;
+
+
+----- to insert a product with zero stock
+INSERT INTO Products(ProductName, CategoryID, Price, Stock)
+VALUES ('Keyboard', 1, 39.99, 0);
+
+--Q6: List all products that are out of stock, where stock = 0
+	SELECT * 
+	FROM Products 
+	WHERE Stock = 0;
+
+-- with category name
+SELECT p.ProductID, p.ProductName, c.CategoryName, p.Stock 
+FROM Products p JOIN Categories c
+ON p.CategoryID = c.CategoryID
+WHERE Stock = 0;
+
+--Q 7: Find customers who placed orders in the last 30 days
+SELECT c.CustomerID, c.FirstName, c.LastName, c.Email, c.Phone
+FROM Customers c JOIN Orders o
+ON c.CustomerID = o.CustomerID
+WHERE o.OrderDate >= DATEADD(DAY, -30, GETDATE());
+
+--Q 8: Retrieve the details of the most recent order
+SELECT TOP 1 o.OrderID, o.OrderDate, o.TotalAmount, c.FirstName, c.LastName
+FROM Orders o JOIN Customers c
+ON o.CustomerID = c.CustomerID
+ORDER BY o.OrderDate DESC;
+
+
+--Q 9: List customers who have never placed an order
+SELECT c.CustomerID, c.FirstName, c.LastName, c.Email, c.Phone, O.OrderID, o.TotalAmount
+FROM Customers c LEFT OUTER JOIN Orders o
+ON c.CustomerID = o.CustomerID
+WHERE o.OrderId IS NULL;
+
+--Q 10: Retrieve the total quantity sold for each product
+SELECT p.ProductID, p.ProductName, SUM(oi.Quantity) AS TotalQuantitySold
+FROM OrderItems oi JOIN Products p
+ON oi.ProductID = p.ProductID
+GROUP BY p.ProductID, p.ProductName
+ORDER BY p.ProductName;
+
+--Q 11: Calculate the total revenue generated from each category
+SELECT c.CategoryID, c.CategoryName, SUM(oi.Quantity * oi.Price) AS TotalRevenue
+FROM OrderItems oi JOIN Products p
+ON oi.ProductID = p.ProductID
+JOIN Categories c
+ON c.CategoryID = p.CategoryID
+GROUP BY c.CategoryID, c.CategoryName
+ORDER BY TotalRevenue DESC;
+
+--Q 12: Find the top 3 most frequently ordered products
+SELECT TOP 3 p.ProductID, p.ProductName, COUNT(oi.OrderID) AS OrderCount
+FROM OrderItems oi JOIN  Products p
+ON oi.ProductID = p.ProductID
+GROUP BY  p.ProductID, p.ProductName
+ORDER BY OrderCount DESC;
+
+--Q 13: Calculate the total number of customers from each country
+SELECT Country, 
+COUNT(CustomerID) AS TotalCustomers
+FROM Customers 
+GROUP BY Country 
+ORDER BY TotalCustomers DESC;
+
+ --Q 14 Display products table
+SELECT * 
+FROM Products;
+
+-- ==========================================================================================
+--                    CRUD operation 
+--  both creat and read operation are used above tring out update operetion 
+--==========================================================================================
+--Q 15 update any one record from Products table
+UPDATE Products SET Price = Price - 300 
+WHERE ProductID = 2;
+
+-- =======================================================================
+--                          CRUD operation 
+--  Now tring performing out DELETE operetion
+--
+--==========================================================================
+
+-- Q 16 delete an existing record ProductID 10
+
+DELETE FROM Products 
+WHERE ProductID = 7;
+
+/*================================================================================================================
+                          THANK YOU
+end
+==================================================================================================================
+*/
